fa6f518009f7b7bf407899b4d6963269e8b30071
Gabriel Medeiros <gabrielcaldeiramedeiros1999@gmail.com>
2026-06-30 10:17:14 -0300

    Update order.py
    Alterações no Args e Returns do order.py

--- order.py
+++ order.py
@@ -1,12 +1,11 @@
 def calculate_total(items: list[float]) -> float:
-    """
-    Calcula e retorna o valor total de uma lista de itens.
+    """Calcula e retorna o valor total de uma lista de itens.
 
     Args:
-        items: Lista de valores dos itens.
+        items: Lista de valores (float) dos itens do pedido.
 
     Returns:
-        Soma dos valores.
+        A soma de todos os valores da lista.
     """
     return sum(items)
 
